2b6d92a1a55d25a8844404d67610e81b96bf016a
Nutchanon Taechasuk <nutchanon.taechasuk@woven-planet.global>
2026-09-08 17:47:54 +0900

    day4

---
+++ crates/2023/src/bin/day4.rs
@@ -0,0 +1,57 @@
+use std::collections::HashSet;
+
+fn main() {
+    let input = aoc::read_input_arg(2023, 4).expect("could not read input");
+
+    let mut sum = 0;
+    let mut card_values = vec![];
+
+    for line in input.lines() {
+        let (_, card) = line.split_once(": ").unwrap();
+
+        let (winning, have) = card.split_once(" | ").unwrap();
+
+        let mut lsum = 0;
+
+        let winning: HashSet<_> = winning
+            .trim()
+            .split_whitespace()
+            .map(|w| w.parse::<u32>().unwrap())
+            .collect();
+
+        let have: HashSet<_> = have
+            .trim()
+            .split_whitespace()
+            .map(|w| w.parse::<u32>().unwrap())
+            .collect();
+
+        for w in winning {
+            if have.contains(&w) {
+                lsum = lsum + 1;
+            }
+        }
+
+        card_values.push(lsum);
+        if lsum > 0 {
+            sum += 1 << lsum - 1
+        }
+    }
+
+    let mut part2 = 0;
+
+    let mut multipliers = vec![1; card_values.len()];
+
+    for i in 0..card_values.len() {
+        let size = card_values[i];
+        for offset in 1..=size {
+            multipliers[i + offset] += multipliers[i];
+        }
+    }
+
+    for i in 0..card_values.len() {
+        part2 += multipliers[i];
+    }
+
+    println!("Part 1: {sum}");
+    println!("Part 2: {part2}");
+}
